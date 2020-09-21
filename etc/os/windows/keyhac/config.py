00127619a7e5e00a092a59eaa60c057f1fef4d6e
n0at <n0a@outlook.jp>
2020-09-22 01:22:05 +0900

    vscodeのキーバインドを変更

--- etc/os/windows/keyhac/config.py
+++ etc/os/windows/keyhac/config.py
@@ -2,5 +2,4 @@ def configure(keymap):
     keymap_global = keymap.defineWindowKeymap()
     keymap_global["S-Semicolon"] = "Semicolon"
     keymap_global["Semicolon"] = "S-Semicolon"
-    keymap_global["O-LCtrl"] = "Esc"
-    keymap_global["O-LShift"] = "Back"
+    keymap_global["LCtrl-j"] = "Esc"
