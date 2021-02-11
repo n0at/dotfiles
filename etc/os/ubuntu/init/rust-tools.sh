6de8ae8b9c12385bed29cf17825769937058a85a
n0at <n0a@outlook.jp>
2021-02-11 20:03:33 +0900

    Add ubuntu/init/rust-tools.sh
    1. rust製ツールをインストールするためのスクリプトを追加

---
+++ etc/os/ubuntu/init/rust-tools.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ ! -z "$(command -v cargo)" ]; then
+    PWD=$(cd $(dirname $0); pwd)
+    sh $PWD/rust.sh
+    source $HOME/.cargo/env
+fi
+
+cargo install ripgrep exa fd bat tokei
