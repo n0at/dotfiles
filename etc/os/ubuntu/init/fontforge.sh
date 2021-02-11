c1b26433b0a0aeec593eb9d456fa520efb839ecc
n0at <n0a@outlook.jp>
2021-02-11 19:15:12 +0900

    Update ubuntu/init/fontforge.sh
    1. インストールするパッケージを変更

--- etc/os/ubuntu/init/fontforge.sh
+++ etc/os/ubuntu/init/fontforge.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# fishのインストール
+# fontforgeのインストール
 if [ -z "$(command -v fontforge)" ]; then
     sudo apt install -y software-properties-common
     sudo add-apt-repository ppa:fontforge/fontforge
     sudo apt update -y
-    sudo apt install -y fontforge python-fontforge
+    sudo apt install -y fontforge python3-fontforge
 fi
