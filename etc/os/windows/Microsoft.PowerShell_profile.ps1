a787928510eb4418b5f8145850acfbc12d6922d9
n0at <n0a@outlook.jp>
2021-05-03 03:31:26 +0900

    Update Microsoft.PowerShell_profile.ps1
    1. キーバインドをEmacs風に変更
    2. Windows Terminal起動時のみoh-my-poshを適用するように変更
    3. lsのエイリアスを変更
    4. catのエイリアスを変更
    5. fzfのキーバインドを追加

--- etc/os/windows/Microsoft.PowerShell_profile.ps1
+++ etc/os/windows/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,15 @@
-Import-Module posh-git
-Import-Module oh-my-posh
-Set-Theme Paradox
+# Windows Terminalで起動した時のみ適用する
+if ($env:WT_PROFILE_ID) {
+    Import-Module posh-git
+    Import-Module oh-my-posh
+    Set-PoshPrompt -Theme aliens
+}
+
+# キーバインドをEmacs風に変更
+Set-PSReadlineOption -EditMode Emacs
+
+# インストールしたコマンドのエイリアスを設定
+Set-Alias ls lsd
+Set-Alias cat bat
+
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
